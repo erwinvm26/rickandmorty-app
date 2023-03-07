a894c83224ab0ac8c72b8ada543955d4a285bfd2
erwinvm26 <erwinmayorga26@gmail.com>
2023-03-07 15:47:17 -0600

    [changes] axios endpoint

--- rickandmorty-project/src/axios.ts
+++ rickandmorty-project/src/axios.ts
@@ -5,10 +5,10 @@ import axios from "axios";
 // })
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000/",
-})
+  baseURL: import.meta.env.VITE_BASE_URL,
+});
 
-export default axiosInstance
+export default axiosInstance;
 
 // export default {
 //   install: (app: App) => {
