d7b67ca3f5ec61ffab174a7a4e64c46d2a41e463
erwinvm26 <erwinmayorga26@gmail.com>
2023-03-07 15:30:01 -0600

    [changes] ormconfig.ts

--- backend-express/ormconfig.ts
+++ backend-express/ormconfig.ts
@@ -2,15 +2,17 @@ import { DataSource } from "typeorm";
 
 import { User } from "./src/module/User/entities/user.entity";
 import { Fav } from "./src/module/ApiRM/entities/fav.entity";
+
+import "dotenv/config";
 // entities: ["dist/src/module/**/entities/*.entity.js"],
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: "secret",
-  database: "postgres",
+  host: process.env.PGHOST,
+  port: +process.env.PGPORT!,
+  username: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
   logging: false,
   synchronize: false,
   name: "default",
